2378bd828133a67fb6bb495b36e0f44d87f3b05e
liskowsky <karollebiedzinski@gmail.com>
2016-07-06 22:25:09 +0100

    Completed 5 times

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it "Returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it "Returns 'fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it "Returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
